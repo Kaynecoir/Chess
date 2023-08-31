ad96ab3c79fe81cc090cf79926d314407e02db53
Kaynecoirir <arseniy.grodno@mail.ru>
2023-08-31 22:37:09 +0300

    new try commit change in branch

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Board
 {
+	List<ChessFigure> whiteFigures, blackFigures;
 	int[,] startBoard =
 	{
 		{ -2, -3, -4, -5, -6, -4, -3, -2 },
@@ -26,4 +27,29 @@ public class Board
 		{  0,  0,  0,  0,  0,  0,  1,  0,},
 		{  0,  0,  0,  0,  0,  0,  0,  0,},
 	};
+	ChessFigure[,] _board = new ChessFigure[8, 8];
+
+	public Board(int[,] intBoard)
+	{
+		whiteFigures = new List<ChessFigure>();
+		blackFigures = new List<ChessFigure>();
+		for (int y = 0; y < 8; y++)
+		{
+			for (int x = y % 2; x < 8; x += 2)
+			{
+				if (intBoard[y, x] > 0)
+				{
+					_board[y, x] = new ChessFigure();
+					whiteFigures.Add(_board[y, x]);
+				}
+				else if (intBoard[y, x] < 0)
+				{
+					_board[y, x] = new ChessFigure();
+					blackFigures.Add(_board[y, x]);
+				}
+			}
+		}
+		Debug.Log(whiteFigures.Count);
+		Debug.Log(blackFigures.Count);
+	}
 }
